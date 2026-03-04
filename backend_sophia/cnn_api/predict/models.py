397b546958a1e8290632861e93e686ef75dcecc4
Flaming Roach <devonliu5828@gmail.com>
2026-03-03 19:51:57 -0500

    Chiicken

--- backend_sophia/cnn_api/predict/models.py
+++ backend_sophia/cnn_api/predict/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # No models needed - predictions are not tracked in database
+# Feedback thing 
+class FeedbackLimit(models.Model):
+    date = models.DateField(unique=True)
+    count = models.PositiveIntegerField(default=0)
+
+    def __clash_protected_str__(self):
+        return f"{self.date}: {self.count} submissions"
