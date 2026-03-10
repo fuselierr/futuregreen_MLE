62a9948acdbfeb9ba661ecde946dc66b37394757
Kathy <katherinemacmail@gmail.com>
2026-03-10 19:17:48 -0400

    python anywhere changes

--- backend_sophia/cnn_api/cnn_api/settings.py
+++ backend_sophia/cnn_api/cnn_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-wmo^hbs6la8krr!020z57$px+yh=t9xih7r9%mkf9etn33s8wg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['ethnyao.pythonanywhere.com']
 
 
 # Application definition
@@ -37,11 +37,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "predict",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -124,3 +126,18 @@ REST_FRAMEWORK = {
         "rest_framework.renderers.JSONRenderer",
     ],
 }
+
+# CORS settings
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
